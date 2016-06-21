248bee7272865fb7387e1d354ccfbd762d669994
Riley Smith <riley.smith@sea.aie.edu>
2016-06-21 16:45:04 -0700

    Player controller

---
+++ Assets/Scripts/Player/Agent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Agent : MonoBehaviour {
+
+    protected float gravity;
+    public float moveSpeed = 6;
+    public float maxJumpHeight = 4;
+    public float minJumpHeight = 1;
+    public float timeToJumpApex = .4f;
+    protected float accelerationTimeAirborne = .2f;
+    protected float accelerationTimeGrounded = .1f;
+
+    protected float maxJumpVelocity;
+    protected float minJumpVelocity;
+    protected Vector3 velocity;
+
+    // Use this for initialization
+    public virtual void Start () {
+        gravity = -(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
