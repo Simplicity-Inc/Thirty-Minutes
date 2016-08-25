ba5364c3d14f6e133674aac9e4128305d57918f1
simplemangames <simplemangames@gmail.com>
2016-08-24 22:55:14 -0700

    Changed the custom inspector for the level blocks asset. Added a drag and drop spot to add new blocks to the list. Added a layer drop down to pick which layer is it being assigned to.

---
+++ Assets/Editor/Custom Inspectors/LevelBlockEditor.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(LevelBlocks))]
+public class LevelBlockEditor : Editor {
+
+    LevelBlocks inspec;
+    int selLayer = 0;
+
+    public void OnEnable() {
+        inspec = (LevelBlocks)target;
+    }
+
+    public override void OnInspectorGUI() {
+        DropAreaGUI();
+        DrawDefaultInspector();
+    }
+
+    public void DropAreaGUI() {
+        Event evt = Event.current;
+        Rect drop_Area = GUILayoutUtility.GetRect(0.0f, 20.0f, GUILayout.ExpandWidth(true));
+        GUI.Box(drop_Area, "New Tile");
+        selLayer = EditorGUILayout.LayerField("Layer: ", selLayer);
+
+        switch(evt.type) {
+            case EventType.DragUpdated:
+            case EventType.DragPerform:
+                if(!drop_Area.Contains(evt.mousePosition)) return;
+
+                DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+
+                if(evt.type == EventType.DragPerform) {
+                    DragAndDrop.AcceptDrag();
+
+                    foreach(Sprite dragObj in DragAndDrop.objectReferences) {
+                        CreatePrefab(dragObj);
+
+                        inspec.Blocks.Add(new LevelBlocksData((GameObject)Resources.Load(dragObj.name)));
+                    }
+                }
+                break;
+        }
+    }
+
+    public void CreatePrefab(Sprite dragObj) {
+        GameObject gO = new GameObject();
+        gO.AddComponent<SpriteRenderer>().sprite = dragObj;
+        gO.name = dragObj.name;
+        gO.AddComponent<BoxCollider2D>();
+        gO.layer = selLayer;
+
+        PrefabUtility.CreatePrefab("Assets/Resources/" + dragObj.name + ".prefab", gO);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+
+        Object.DestroyImmediate(gO);
+
+    }
+}
--- Assets/Scripts/Map/Tile Set/LevelBlock.cs
+++ Assets/Scripts/Map/Tile Set/LevelBlock.cs
@@ -5,8 +5,13 @@ using UnityEditor;
 
 [System.Serializable]
 public class LevelBlocksData {
-    public string Name;
+    public string Name = "";
     public GameObject Prefab;
+
+    public LevelBlocksData(GameObject prefab) {
+        Name = prefab.name;
+        Prefab = prefab;
+    }
 }
 
 [CreateAssetMenu]
